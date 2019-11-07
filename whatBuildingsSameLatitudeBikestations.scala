72d9598a295bbc77bf1372575ab2346e0bfcde68
Anders Saxrud <brannersrud@gmail.com>
2019-11-07 11:00:25 +0100

    Updated the lon/lat comparison to actually

--- whatBuildingsSameLatitudeBikestations.scala
+++ whatBuildingsSameLatitudeBikestations.scala
@@ -13,37 +13,35 @@ object whatBuildingsSameLatitudeBikestations {
     sc.setLogLevel("ERROR")
     val sparkmain = SparkSession.builder().getOrCreate()
 
-    val latBikes = getLatitudeBikes()
-    val nodeDf= getLatitudeBuildings()
+    val latBikes = getLatitudeBikes(sparkmain)
+    val nodeDf= getLatitudeBuildings(sparkmain)
+
     val joined = latBikes.crossJoin(nodeDf)
-   calculateTotalDistances(joined, sparkmain)
+    removeBuildingsThatAreFarAway(joined, sparkmain)
   }
 
-  def calculateTotalDistances(df: DataFrame, sparkSession: SparkSession) {
+  def removeBuildingsThatAreFarAway(df: DataFrame, sparkSession: SparkSession) {
     import sparkSession.implicits._
-    val latDiff = df.filter(($"start_bike_lat".cast(DoubleType) % $"_lat".cast(DoubleType) < 0.0005) && $"end_bike_lon".cast(DoubleType) % $"_lon".cast(DoubleType) < 0.0005)
-    val filterKiellands = latDiff.filter($"start_station_name" === "Alexander Kiellands Plass")
-    latDiff.show()
+    val latDiff = df.filter(($"start_bike_lat".cast(DoubleType) % $"_lat".cast(DoubleType) < 0.005) && $"end_bike_lon".cast(DoubleType) % $"_lon".cast(DoubleType) < 0.005)
+    latDiff.show(10000)
   }
 
-  def getLatitudeBikes() : DataFrame = {
-    val spark = SparkSession.builder().getOrCreate()
+  def getLatitudeBikes(spark: SparkSession) : DataFrame = {
     import spark.implicits._
     val df = spark.read.format("csv")
       .option("header", "true")
       .option("inferSchema", "true")
       .load("input/bikeextractseptember2019.csv")
 
-    val lat = df.select( $"start_station_name").groupBy( $"start_station_name").count()
-    val lon = df.select(round($"start_station_longitude", 6).as("end_bike_lon"), round($"start_station_latitude", 6).as("start_bike_lat"))
-    val joined = lat.crossJoin(lon)
+    val lat = df.select( $"start_station_id").groupBy( $"start_station_id").count()
+    val lon = df.select($"start_station_id", $"start_station_name",round($"start_station_longitude", 6).as("end_bike_lon"), round($"start_station_latitude", 6).as("start_bike_lat"))
+    val joined = lat.join(lon, lat("start_station_id") === lon("start_station_id"))
 
    joined
   }
 
 
-  def getLatitudeBuildings(): DataFrame = {
-    val spark = SparkSession.builder().getOrCreate()
+  def getLatitudeBuildings(spark : SparkSession): DataFrame = {
     import spark.implicits._
     val dfOsm = spark.read.format("com.databricks.spark.xml")
       .option("rootTag", "osm")
@@ -53,10 +51,10 @@ object whatBuildingsSameLatitudeBikestations {
     val latBuild = dfOsm.select($"_id", $"_lon", $"_lat", explode($"tag").as("buildingTags"))
     val withcolumn = latBuild.withColumn("Latitude_building", round($"_lat".cast(DoubleType), 6))
     val lonbuild = withcolumn.withColumn("Long_building", round($"_lon".cast(DoubleType), 6))
-    //val filteredBuilding = lonbuild.filter($"buildingTags._k" === "building")
+    val filteredBuilding = lonbuild.filter($"buildingTags._k" === "building")
 
 
-    lonbuild
+    filteredBuilding
   }
 
 
